a9f36b583eb49f97d3dad1e46acbae54d0d196fd
mawenzhong <546591791@qq.com>
2015-07-11 23:49:21 +0800

    dddd

---
+++ webview/src/main/java/com/example/webview/webview/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.webview.webview;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+
+public class MainActivity extends Activity {
+
+    private WebView webView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.getActionBar().hide();
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        //webView.getSettings().setPluginState(PluginState.ON);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(true);
+        //webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        //webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+/*
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        }
+*/
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.i("shit", url);
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+                Log.i("fuck", url);
+                WebResourceResponse response = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                    response = super.shouldInterceptRequest(view, url);
+                    if (url.contains("icon.png")) {
+                        try {
+                            response = new WebResourceResponse("image/png", "UTF-8", getAssets().open("icon.png"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+//              return super.shouldInterceptRequest(view, url);
+                return response;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.loadUrl("javascript:shit()");
+
+            }
+
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                super.onLoadResource(view, url);
+                Log.i("Resource", url);
+            }
+        });
+
+
+        webView.addJavascriptInterface(new Object() {
+
+            @JavascriptInterface
+            public void alert(String msg) {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+
+        }, "android");
+
+        webView.loadUrl("file:///android_asset/Framework7/index.html");
+
+        //webView.loadUrl("http://www.iqiyi.com/");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
